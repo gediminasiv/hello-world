9135e0a1ae4d4d543c13c78643ede7d355490955
Trubaduras <fordainius@gmail.com>
2015-04-07 19:59:43 +0300

    Created new login.php

---
+++ login.php
@@ -0,0 +1,15 @@
+<div class="container">	
+	<div class="jumbotron">
+		<form class="form-horizontal" role="form">
+			<div class="form-group">	
+				<label for="text">User Name</label>
+				<input class="form-control" type="text" name="name" placeholder="Your name" />
+			</div>
+			<div class="form-group">
+				<label for="password">Password</label>
+				<input class="form-control" type="password" name="password" placeholder="Password"  />
+			</div>
+			<input type="submit" class="btn btn-warning" value="Login" name="register" />
+		</form>
+	</div> 
+</div>
