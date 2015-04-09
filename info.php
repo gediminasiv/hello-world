f123d79ae03791c168f7c9ecc2a216e82d65f76a
Trubaduras <fordainius@gmail.com>
2015-04-09 15:01:40 +0300

    Update info.php
    User remove button not working

--- info.php
+++ info.php
@@ -1,3 +1,15 @@
+	<style>
+		.edit {
+			
+			margin-right:5%;
+		}
+		.remove {
+			
+			margin-left:5%;
+			
+		}
+    </style>
+	<html>
 	<div class="container">
 	<div class="jumbotron">
 	<div class="table-scrol">  
@@ -13,39 +25,57 @@
 			<th>User Name</th>  
 			<th>User E-mail</th>  
 			<th>User Pass</th>
-			<th>User Id</th>			 
+			<th>User Id</th>
+			<th>Edit</th>
 		</tr>  
 		</thead>  
   
 		<?php  
 
-		$link = mysqli_connect("localhost","root","123456", "useriai");		
+		$link = mysqli_connect("mysql1.000webhost.com","a8715883_turbo","Qwe357", "a8715883_users");		
 		$view_users_query="select * from user";
 		$run=mysqli_query($link,$view_users_query);
 		while($row=mysqli_fetch_array($run))
 		{  
+		$user_id = array();
+		$i = 1;
 			$user_name=$row[0];  
 			$user_email=$row[1];  
 			$user_pass=$row[2];  
-			$user_id=$row[3];
+			$user_id[$i]=$row[3];
   
   
 		?>  
   
 		<tr>  
-			<td><?php echo $user_id;  ?></td>  
 			<td><?php echo $user_name;  ?></td>  
 			<td><?php echo $user_email;  ?></td>  
-			<td><?php echo $user_pass;  ?></td>  
+			<td><?php echo $user_pass;  ?></td>
+			<td><?php echo $user_id[$i];  ?></td>
+			<td>
+			<form method="post">
+				<button type="submit" class="btn btn-info" name="userid" value="$user_id[$i]"> <span class="glyphicon glyphicon-pencil" aria-label="Edit"></span></button>
+				<a href="remove.php" data-toggle="modal" data-target="#removeModal"><button type="button" class="remove btn btn-danger"><span class="glyphicon glyphicon-remove" aria-label="Remove"></span></button></a>
+			</form>
+			</td>
 		</tr>  
-		<?php } ?>  
+		<?php  
+		
+		$i++;		
+		}
+		if(isset($_POST['userid']))
+		{
+			$sql="DELETE FROM user WHERE id=".$_POST['userid'];
+			$res=mysqli_query($link,$sql);
+			
+		}		?>  
   
 	</table>  
 	</div>  
 	</div> 
 	</div>
 	</div>
-  
+	
 </body>  
   
 </html>  
