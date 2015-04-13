c078979142d417bfe94ec09e0c66734f03324c6e
Trubaduras <fordainius@gmail.com>
2015-04-13 15:33:48 +0300

    Update edit.php
    php doesn't do it's job when save changes is pressed.

--- edit.php
+++ edit.php
@@ -4,22 +4,43 @@
 		<h4 class="modal-title">Edit Window</h4>
 	</div>
 	<div class="modal-body">
-		<div class="container">	
+		<div class="container-fluid">
 		<form class="form-horizontal" role="form" method="POST">
+		<div class="form-group">
+			<label for="sel1">Select Id</label>
+			<select class="form-control" name="selectId">
+			<?php  
+				$i=1;
+				$link = mysqli_connect("mysql1.000webhost.com","a8715883_turbo","Qwe357", "a8715883_users");		
+				$view_users_query="select * from user";
+				$run=mysqli_query($link,$view_users_query);
+				while($row=mysqli_fetch_array($run))
+				{
+					$i++;
+					$user_name[$i]=$row[0];  
+					$user_email[$i]=$row[1];  
+					$user_pass[$i]=$row[2];  
+					$user_id[$i]=$row[3];
+			?>
+			<option value="<?=$user_id[$i]?>"><?php echo $user_id[$i];  ?></option>	
+				<?php } ?>
+				</select>
+			</div>
+			
 			<div class="form-group">	
 				<label for="text">User Name</label>
-				<input class="form-control" type="text" name="name" placeholder="<?=$user_name[$i]?>" />
+				<input class="form-control" type="text" name="name" placeholder="User Name" />
 			</div>
 			<div class="form-group">
 				<label for="password">Email</label>
-				<input class="form-control" type="email" name="email" placeholder="<?=$user_email[$i]?>"  />
+				<input class="form-control" type="email" name="email" placeholder="Email"  />
 			</div>
 			<div class="form-group">
 				<label for="password">Password</label>
-				<input class="form-control" type="password" name="password" placeholder="<?=$user_pass[$i]?>"  />
+				<input class="form-control" type="password" name="password" placeholder="Password"  />
 			</div>
 		
-	</div> 
+		</div> 
 </div>
 	</div>
 	<div class="modal-footer">
@@ -30,11 +51,11 @@
 	<?php
 		
 		$link = mysqli_connect("mysql1.000webhost.com","a8715883_turbo","Qwe357", "a8715883_users");
-		$sql="UPDATE user SET name='".$_POST['name']."' WHERE id=".$_POST['id'];
+		$sql="UPDATE user SET name='".$_POST['name']."' WHERE id=".$_POST['selectId'];
 		$res=mysqli_query($link,$sql);
-		$sql="UPDATE user SET email='".$_POST['email']."' WHERE id=".$_POST['id'];
+		$sql="UPDATE user SET email='".$_POST['email']."' WHERE id=".$_POST['selectId'];
 		$res=mysqli_query($link,$sql);
-		$sql="UPDATE user SET password='".$_POST['password']."' WHERE id=".$_POST['id'];
+		$sql="UPDATE user SET password='".$_POST['password']."' WHERE id=".$_POST['selectId'];
 		$res=mysqli_query($link,$sql);
 	?>
 	
